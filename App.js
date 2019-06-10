3018b1a995cf1673c00f8a3c02ada20a1fbd09b2
AmadorZcv <lucasamadorzcv@gmail.com>
2019-06-10 17:54:11 -0300

    Redux ok

--- App.js
+++ App.js
@@ -6,39 +6,20 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+import store from './app/redux/store'
 import Tabs from './app/config/routes';
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+
 
 export default class App extends Component {
   render() {
     return (
-      <Tabs/>
+      <Provider store={store}>
+        <Tabs />
+        </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
